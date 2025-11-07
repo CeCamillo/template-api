a08f8a9dd96e3a40c8d1a09aabb5e3a6194c9b0c
Cesar Henrique Batista Camillo <cesarcamillo@MacBook-Air-de-Cesar.local>
2025-11-07 00:16:59 -0300

    docker and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.use(
+    cors({
+      origin: "http://localhost:3000", // Only allow your frontend
+      credentials: true, // We'll need this for cookies later
+    })
+  ) // Use it here
+  .get("/", () => "Hello Elysia")
+  .get('/health', () => {
+    return {
+      status: 'ok',
+      message: "API is running!"
+    }
+  })
+  .listen(3001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
